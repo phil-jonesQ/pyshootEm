c31d09506e60d8d6897aa84bf27cb2202e424e3e
phil-jonesQ <phil.jones.24.4@gmail.com>
2019-07-27 12:18:58 +0100

    implementing movement of ship

--- shoot_em.py
+++ shoot_em.py
@@ -20,6 +20,7 @@ GREEN = (0, 255, 0)
 LIGHT_GREEN = (101, 152, 101)
 GREY = (128, 128, 128)
 
+
 # Load images
 BG = pygame.transform.scale(pygame.image.load("game_assets/BG.jpg"), (WindowWidth, WindowHeight))
 ship = pygame.transform.scale(pygame.image.load("game_assets/Ship1_new.png"), (200, 40))
@@ -40,12 +41,25 @@ class Ship(object):
 
     def __init__(self, start_pos):
         self.ship_pos = start_pos
-
-    def draw(self, surface):
-
         self.ship_pos_x = self.ship_pos[0]
         self.ship_pos_y = self.ship_pos[1]
-        surface.blit(ship, self.ship_pos)
+        self.mover = 0
+        self.velocity = 1
+
+    def draw(self, surface):
+        the_ship_pos = self.ship_pos
+        #print(the_ship_pos)
+        surface.blit(ship, the_ship_pos)
+
+    def move(self, ship_dir):
+        ship_velocity = self.velocity
+        print(ship_velocity,self.mover)
+        if ship_dir == 1:
+            self.mover += ship_velocity
+            ship_velocity += 10
+        if ship_dir == -1:
+            self.mover -= ship_velocity
+        self.ship_pos = (self.ship_pos_x, self.ship_pos_y + self.mover)
 
 
 class Asteroid(object):
@@ -57,6 +71,7 @@ def main():
     pygame.init()
     shoot_em_surface = pygame.display.set_mode((WindowWidth, WindowHeight))
     pygame.display.set_caption("Shoot Em " + str(version))
+    pygame.key.set_repeat(1, 1)
 
     # Initialise fonts we will use
     font = pygame.font.SysFont('Arial', 50, False, False)
@@ -82,7 +97,7 @@ def main():
 
 
         # Control frame rate
-        clock.tick(60)
+        clock.tick(160)
 
         # Handle user input
         for event in pygame.event.get():
@@ -90,14 +105,13 @@ def main():
                 loop = False
                 pygame.quit()
 
-            # Don't react to letter keys if the game is over
-            if event.type == pygame.KEYDOWN and not game_over:
-                if event.key == pygame.K_UP:
-                    print("UP")
-                if event.key == pygame.K_DOWN:
-                    print("DOWN")
-                if event.key == pygame.K_SPACE:
-                    print ("FIRE")
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            s.move(-1)
+        if keys[pygame.K_DOWN]:
+            s.move(1)
+        if keys[pygame.K_SPACE]:
+            print("FIRE")
 
 # Call main
 if __name__ == "__main__":
